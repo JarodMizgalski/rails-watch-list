fd6cc7fc7bb1d4b619d631f534cebd4867b23d2e
JarodMizgalski <jarodmiz2018@gmail.com>
2024-11-14 13:09:32 +0900

    added model, db, validations

---
+++ app/models/movie.rb
@@ -0,0 +1,6 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true, uniqueness: true
+  # validates :rating, presence: true, numericality: { only_float: true, in: 0..10 }
+end
