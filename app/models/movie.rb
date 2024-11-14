838d59f1e5842ecf0897f368d0655998e7ee9cdf
JarodMizgalski <jarodmiz2018@gmail.com>
2024-11-14 16:58:38 +0900

    finished basica function of lists

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,5 @@ class Movie < ApplicationRecord
   has_many :bookmarks
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true, uniqueness: true
-  # validates :rating, presence: true, numericality: { only_float: true, in: 0..10 }
+  # validates :rating, presence: true, numericality: { only_float: true, in: 0.0..10.0 }
 end
